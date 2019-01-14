7cacf1a95eacd664abc5e601c224242eee42ff86
Radoslav <radoslav@math.cornell.edu>
2019-01-14 11:00:18 -0500

    RZ-LOCAL adding local main
    (cherry picked from commit f034bbd0ca77e0214bd27f3747d0c4b358960cd4)

--- M2/Macaulay2/d/main.c
+++ M2/Macaulay2/d/main.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+
 extern int Macaulay2_main(int argc, char **argv);
+extern char timestamp[];
 
 int main(int argc, char **argv)
 {
+  printf("*** RZ's test code! timestamp is: %s\n\n", timestamp);
   return Macaulay2_main(argc, argv);
 }
